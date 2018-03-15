a2e93e999e8d214b4b804889fc89eada09fb82d9
luizpaulofranz <luizpaulofranz@gmail.com>
2018-03-15 08:36:18 -0300

    Getters from Vuex object.

---
+++ src/store/getters.js
@@ -0,0 +1,23 @@
+// ./src/store/getters
+/**
+ * Essa classe implementa os getters do nosso objeto Vuex, declarado no ./index
+ */
+export const productGetters = {
+    // All products
+    allProducts: (state, getters) => {
+      return state.products
+    },
+    // Get Product by ID
+    productById: (state, getters) => id => {
+      if (getters.allProducts.length > 0) {
+        return getters.allProducts.filter(p => p._id === id)[0]
+      } else {
+        return state.product
+      }
+    },
+  }
+  
+  export const manufacturerGetters = {
+    // All manufacturers
+    allManufacturers: state => state.manufacturers
+  }
