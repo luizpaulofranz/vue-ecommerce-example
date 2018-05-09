cd84253afb2a27bca445984945153829a27523c8
luizpaulofranz <luizpaulofranz@gmail.com>
2018-05-09 17:20:18 -0300

    Adds Cart page.

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
+  <div v-if="cart.length > 0">
     <div class="title">
-      <h1>{{msg}}</h1>
+      <h1><i class="fa fa-superpowers"></i> Seu Carrinho</h1>
     </div>
+    <template v-for="product in cart">
+      <product-details :product="product" :key="product.id"></product-details>
+    </template>
   </div>
+  <div v-else class="title"><h1><i class="fa fa-superpowers"></i> Your Cart is Empty</h1></div>
 </template>
 
 <script>
+  import ProductDetails from '../components/product/ProductDetail'
   export default {
     name: 'cart',
     data () {
       return {
-        msg: 'Bem vindo ao seu Carrinho'
+        cart: this.$store.state.cart
       }
+    },
+    components: {
+      productDetails: ProductDetails
     }
   }
 </script>
