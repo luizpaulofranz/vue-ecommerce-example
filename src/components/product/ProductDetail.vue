3eb1b2bbdae0a7c67843f37d1b4a5068ea626500
luizpaulofranz <luizpaulofranz@gmail.com>
2018-05-09 17:18:19 -0300

    Adds style to product details page.

--- src/components/product/ProductDetail.vue
+++ src/components/product/ProductDetail.vue
@@ -16,7 +16,7 @@
           </div>
           <div class="product-details__price-cart">
             <p>${{product.price}}</p>
-            <product-button :product="product" ></product-button>
+            <product-button :product="product"></product-button>
           </div>
         </div>
       </div>
@@ -33,3 +33,37 @@
     }
   }
 </script>
+
+<style>
+  .product-details {
+    border-bottom: 2px solid #F5F5F5;
+    padding: 30px 0;
+  }
+
+  .product-details__image {
+
+  }
+
+  .product-details__description {
+    padding-top: 30px;
+  }
+
+  .product-details__description small {
+    color: #808080;
+  }
+
+  .product-details__description h3 {
+    margin: 5px 0 30px 0;
+  }
+
+  .product-details__price-cart {
+    display: flex;
+    padding-top: 30px;
+  }
+
+  .product-details__price-cart p {
+    flex-grow: 2;
+    font-size: 20px;
+    font-weight: bold;
+  }
+</style>
